3a63f7914a953e308a441b129ac90372133b3c60
josiascardosodias-glitch <josiascardosodias@gmail.com>
2025-09-15 18:12:14 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# biblioteca_uteis
+# biblioteca_úteis
 bliblioteca de funções para auxiliar iniantes em seus primeiros projetos
+
+( Esse projeto ainda esta em desenvolvimento, por esse motivo não está em formato de biblioteca e sim de módulo. Porém, no momento em que todas as funções forem adicionadas ao repositório. Eu irei reorganizar o repositório e assim transforma-lo de fato em uma bibliotaca. )
---
+++ biblioteca_uteis(versão incompleta)/uteis.py
@@ -0,0 +1,43 @@
+def linha(tamanho=1):
+    print( '-' * tamanho)
+
+def cabeçalho(texto):
+    linha(len(texto) + 4)
+    print(f'  {texto}')
+    linha(len(texto) + 4)
+    
+def leiaInt(msg):
+    valor = False
+    while not valor:
+        n = str(input(msg)).strip()
+        if n.isnumeric():
+            return int(n)
+            break
+        else:
+            print(f'\033[1;31mERRO! o valor {n} não é um número inteiro, Digite novamente.\033[m')
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg).replace(',', '.'))
+        except (ValueError, TypeError):
+            print(f'\033[1;31mERRO! o valor digitado não é um número real, Digite novamente.\033[m')
+        except KeyboardInterrupt:
+            print(f'\n\033[1;31mO usuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+def real(valor=0, moeda='R$'):
+    return f'{moeda}{valor:>.2f}'.replace('.', ',')
+
+def dollar(valor=0, moeda='US$'):
+    return f'{moeda}{valor:>.2f}'.replace('.', ',')
+
+def aumentar(valor=0, taxa=0):
+    return valor + (valor * taxa / 100)
+
+def diminuir(valor=0, taxa=0):
+    return valor - (valor * taxa / 100)
+
+print(f'um salário de {real(1000)} com aumento de 10% é de {real(aumentar(1000, 10))}')
